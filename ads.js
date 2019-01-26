78e1f283d9f23ed66fe750096f2ac9301debc984
Kundan <iamkundankumar28@gmail.com>
2019-01-26 10:02:47 +0530

    Added AdTagURL

--- ads.js
+++ ads.js
@@ -36,4 +36,4 @@ function onContentResumeRequested() {
     //console.log(" ad skipped ");
     videoContent.style.display = "none", adContent.style.display = "none"
 }
-adsRequest.adTagUrl = "place tag here", adsRequest.linearAdSlotWidth = 640, adsRequest.linearAdSlotHeight = 400, adsRequest.nonLinearAdSlotWidth = 640, adsRequest.nonLinearAdSlotHeight = 150;
+adsRequest.adTagUrl = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=", adsRequest.linearAdSlotWidth = 640, adsRequest.linearAdSlotHeight = 400, adsRequest.nonLinearAdSlotWidth = 640, adsRequest.nonLinearAdSlotHeight = 150;
